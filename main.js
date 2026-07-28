6dd847db58de2abd8f45db6839897c200162573a
lucazinho1002 <lucazinho1002@gmail.com>
2026-07-28 19:45:52 -0300

    codigo fonte

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    title: "Aqua AI",
+    backgroundColor: '#030712',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.setMenuBarVisibility(false);
+  win.loadFile(path.join(__dirname, 'src', 'index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
